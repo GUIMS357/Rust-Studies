63807843beec9b11df8f01a56b48b3d3afe69476
GUIMS357 <guims357@gmail.com>
2022-08-23 11:18:58 -0300

    add repetitions

---
+++ Repetitions.rs
@@ -0,0 +1,14 @@
+let mut a = 0;
+loop{
+    if a == 5;{
+        break;
+    }
+    println!("{:?}", a);
+    a = a + 1;
+}
+
+let mut b =0;
+while b != 5{
+    println!("{:?}", b);
+    b = b + 1;
+}
