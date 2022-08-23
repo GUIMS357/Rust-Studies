af0c99e1db88094010098c08b436513d8d26d356
GUIMS357 <guims357@gmail.com>
2022-08-23 11:32:26 -0300

    add mathoperations

---
+++ mathoperations.rs
@@ -0,0 +1,16 @@
+fn fsub (a: i32,b: i32)-> i32{
+    a-b
+}
+
+fn main()
+{
+    let sum = 2 + 2;
+    let sub = 10 - 5;
+    let mult = 5 * 5;
+    let div = 10 / 2;
+
+    let five = fsub(8,3);
+
+    let rem = 6 % 3;
+    let rem2 = 6 % 4;
+}
